05771b1c1c4ffcb847bf4b80444d6c3f163a21ba
Quang Nguyen Dang <quangnguyendang@Quangs-MacBook-Air-2.local>
2023-08-12 10:42:07 +0700

    add chart for pron scores

--- pages/1_👤_Teacher.py
+++ pages/1_👤_Teacher.py
@@ -5,6 +5,8 @@ from yaml.loader import SafeLoader
 from supabase import create_client, Client
 import requests
 from io import BytesIO
+import matplotlib.pyplot as plt
+from collections import defaultdict
 
 st.set_page_config(page_title="Teacher", page_icon="👤")
 
@@ -83,6 +85,7 @@ if st.session_state["authentication_status"]:
                 mispronunciations.extend(item['mispronunciation'].split(', '))
             fluency_scores.append(float(item['fluency_score']))
             pron_scores.append(float(item['pron_score']))
+
             st.divider()
         
         st.write('#### Average pronunciation scores')
@@ -93,6 +96,31 @@ if st.session_state["authentication_status"]:
 
         average_pronunciation = sum(pron_scores) / len(pron_scores)
         col2.metric("Pronunciation", f"{average_pronunciation:.1f}")
+
+        # Gather scores in a dictionary, keyed by lesson number, and convert "Lesson #" to #
+        scores_by_lesson = defaultdict(lambda: {'fluency': [], 'pron': []})
+        for item in [d for d in user_and_lesson_data if d['name'] == selected_name and d['user_class'] == selected_user_class]:
+            lesson_number = int(item['lesson_number'].split(" ")[1])  # "Lesson #" to #
+            scores_by_lesson[lesson_number]['fluency'].append(float(item['fluency_score']))
+            scores_by_lesson[lesson_number]['pron'].append(float(item['pron_score']))
+
+        # Compute average scores for each lesson
+        avg_fluency_by_lesson = [sum(val['fluency'])/len(val['fluency']) for key, val in sorted(scores_by_lesson.items())]
+        avg_pron_by_lesson = [sum(val['pron'])/len(val['pron']) for key, val in sorted(scores_by_lesson.items())]
+
+        fig, ax = plt.subplots()
+
+        # Plot average fluency and pronunciation scores over lessons
+        ax.plot(range(1, len(avg_fluency_by_lesson) + 1), avg_fluency_by_lesson, 'o-', label='Avg Fluency scores')
+        ax.plot(range(1, len(avg_pron_by_lesson) + 1), avg_pron_by_lesson, 'o-', label='Avg Pron scores')
+
+        ax.legend()
+        ax.set_xlabel('Lesson')
+        ax.set_ylabel('Average Score')
+        ax.set_title(f'Average scores over lessons for {selected_name}')
+
+        st.pyplot(fig)
+
 
         st.write('#### Potential mispronunciations')
         st.write(", ".join(mispronunciations))
